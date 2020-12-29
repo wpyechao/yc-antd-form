426a17156df9b40e466cf745618214ed3e496736
wpyechao <pingyechao820@live.com>
2020-12-29 23:02:05 +0800

    fix: rename

--- src/reset/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { ButtonProps } from 'antd/es/button';
-declare const Reset: React.FC<ButtonProps>;
-export default Reset;
--- src/submit/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { ButtonProps } from 'antd/es/button';
-declare const Submit: React.FC<ButtonProps>;
-export default Submit;
